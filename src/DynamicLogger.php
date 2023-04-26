41a480bfe514ee4a54ec1bf14571abaca4f9ff45
zingimmick <zingimmick@outlook.com>
2023-04-27 00:11:42 +0800

    Implementation of PSR LoggerInterface

---
+++ src/DynamicLogger.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zing\YiiPsrLogger;
+
+use Psr\Log\AbstractLogger;
+
+class DynamicLogger extends AbstractLogger
+{
+    /**
+     * @var \Zing\YiiPsrLogger\Logger|null
+     */
+    private $logger;
+
+    /**
+     * @var \yii\log\Logger|null
+     */
+    private $yiiLogger;
+
+    /**
+     * @var string
+     */
+    private $category;
+
+    public function __construct(string $category = 'application')
+    {
+        $this->category = $category;
+    }
+
+    private function getLogger(): Logger
+    {
+        if (! ($this->logger !== null && $this->yiiLogger !== null) || \Yii::getLogger() !== $this->yiiLogger) {
+            $this->yiiLogger = \Yii::getLogger();
+            $this->logger = new Logger(\Yii::getLogger(), $this->category);
+        }
+
+        return $this->logger;
+    }
+
+    /**
+     * @param string $level
+     * @param \Stringable|string $message
+     */
+    public function log($level, $message, array $context = []): void
+    {
+        $this->getLogger()
+            ->log($level, $message, $context);
+    }
+}
---
+++ tests/DynamicLoggerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zing\YiiPsrLogger\Tests;
+
+use yii\log\Logger as YiiLogger;
+use Zing\YiiPsrLogger\DynamicLogger;
+
+/**
+ * @internal
+ */
+final class DynamicLoggerTest extends TestCase
+{
+    public function testLoggerUsesCurrent(): void
+    {
+        $mock = $this->getMockBuilder(YiiLogger::class)->getMock();
+        $mock->expects($this->once())
+            ->method('log')
+            ->with('test1 []', YiiLogger::LEVEL_INFO);
+
+        $yiiLogger2 = $this->getMockBuilder(YiiLogger::class)->getMock();
+        $yiiLogger2->expects($this->once())
+            ->method('log')
+            ->with('test2 []', YiiLogger::LEVEL_INFO);
+
+        $dynamicLogger = new DynamicLogger();
+        \Yii::setLogger($mock);
+
+        $dynamicLogger->info('test1');
+
+        \Yii::setLogger($yiiLogger2);
+        $dynamicLogger->info('test2');
+    }
+}

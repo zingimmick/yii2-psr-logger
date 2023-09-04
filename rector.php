cc3abbe4a97e6e29f3799362b7a787a3f54df920
zingimmick <zingimmick@outlook.com>
2023-09-04 22:03:23 +0800

    Fix the namespace of AddSeeTestAnnotationRector

--- rector.php
+++ rector.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Zing\CodingStandard\Set\RectorSetList;
@@ -12,10 +12,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([LevelSetList::UP_TO_PHP_73, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
     $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
     $rectorConfig->parallel();
-    $rectorConfig->skip(
-        [
-            AddSeeTestAnnotationRector::class,
-        ]
-    );
+    $rectorConfig->skip([AddSeeTestAnnotationRector::class]);
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };

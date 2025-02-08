1479591cd0de670f8a61ff234e36f00ea6c14086
Zing <zingimmick@outlook.com>
2025-02-08 15:46:33 +0800

    Update Logger.php

--- src/Logger.php
+++ src/Logger.php
@@ -39,7 +39,7 @@ class Logger extends AbstractLogger
 
     /**
      * @param mixed $level
-     * @param string|\Zing\YiiPsrLogger\Stringable $message
+     * @param string|\Stringable $message
      * @param array<mixed> $context
      */
     public function log($level, $message, array $context = []): void

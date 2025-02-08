ae759fb83dcd4383c181c7ce9414940193ccca21
zingimmick <zingimmick@users.noreply.github.com>
2025-02-08 07:45:50 +0000

    Rector Rectify

--- src/Logger.php
+++ src/Logger.php
@@ -39,7 +39,7 @@ class Logger extends AbstractLogger
 
     /**
      * @param mixed $level
-     * @param string|Stringable $message
+     * @param string|\Zing\YiiPsrLogger\Stringable $message
      * @param array<mixed> $context
      */
     public function log($level, $message, array $context = []): void

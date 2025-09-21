6385e65f98ead1e4fdc25d7c037fe7b2ca57da2a
Zing <zingimmick@outlook.com>
2025-09-21 20:19:02 +0800

    Update LoggerTest.php

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -13,7 +13,7 @@ use Zing\YiiPsrLogger\Logger;
  */
 final class LoggerTest extends TestCase
 {
-    public function testLogLevelMap(): void
+    public function testLogLevelMap(): void 
     {
         $mock = $this->getMockBuilder(YiiLogger::class)->getMock();
         $mock->expects($this->once())
